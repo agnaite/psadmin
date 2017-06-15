431f7871222459b9bb74ef4e644506d71ea40190
Agne Klimaite <agnaite@me.com>
2017-06-15 13:42:44 -0700

    added home page component

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className='jumbotron'>
+        <h1>PS Administration</h1>
+        <p>Tutorial</p>
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
